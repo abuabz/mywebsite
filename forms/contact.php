2ad00e52325d06cfbddbdd3473d8380bd5f150f8
MOHAMED ABOOBACKER P <111522756+abuabz@users.noreply.github.com>
2023-11-08 12:13:17 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -20,7 +20,7 @@
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
+  $contact->from_email = 'mohamedaboobackerp@gmail.com';
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
